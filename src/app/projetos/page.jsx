797dc2fb452181e5374691a1b95e0faaf9f58d85
walacecordeiro <walacecordeirodossantos@gmail.com>
2025-05-06 16:45:33 -0300

    avançando nas requisições de repositório

--- src/app/projetos/page.jsx
+++ src/app/projetos/page.jsx
@@ -3,41 +3,47 @@ import Card from "../../components/_components/Card";
 import { data } from "./data";
 import TextLink from "@/components/_components/TextLink";
 import { GoProjectSymlink } from "react-icons/go";
+import { fetchUserRepos } from "@/services/github";
 
-export default function ProjectsPage() {
- return (
-  <SectionSticky title="Projetos em Destaque" className="lg:pt-14">
-   <ul className="group/list">
-    {data
-     .filter((project) => project.highlight)
-     .map((project) => (
-      <Card
-       key={project.id}
-       href={project.href}
-       innerImage={project.images[0]}
-       innerTitle={project.innerTitle}
-       innerDescription={project.innerDescription}
-       tags={project.tags}
-      />
-     ))}
-    <SectionSticky title="Projetos">
-     {data
-      .filter((project) => project.highlight === false)
-      .map((project) => (
-       <Card
-        key={project.id}
-        href={project.href}
-        innerImage={project.images[0]}
-        innerTitle={project.innerTitle}
-        innerDescription={project.innerDescription}
-        tags={project.tags}
-       />
-      ))}
+export default async function ProjectsPage() {
+  const profileRepos = await fetchUserRepos(
+    process.env.NEXT_PUBLIC_GITHUB_USER
+  );
+  
+  const filter = profileRepos.filter(repo => repo.description != null & repo.visibility == "public")
+  console.log(filter);
+
+  return (
+    <SectionSticky title="Projetos em Destaque" className="lg:pt-14">
+      <ul className="group/list">
+        {profileRepos
+          .filter((repo) => repo.description != null & repo.private != true)
+          .map((repo) => (
+            <Card
+              key={repo.id}
+              href={repo.html_url}
+              innerTitle={repo.full_name}
+              innerDescription={repo.description}
+              tags={repo.topics}
+            />
+          ))}
+        <SectionSticky title="Projetos">
+          {profileRepos
+            .filter((repo) => repo.highlight === false)
+            .map((repo) => (
+              <Card
+                key={repo.id}
+                href={repo.html_url}
+                innerTitle={repo.full_name}
+                innerDescription={repo.description}
+                tags={repo.topics}
+              />
+            ))}
+        </SectionSticky>
+      </ul>
+      <TextLink innerText="Ver todos os projetos">
+        <GoProjectSymlink className="inline-block h-4 w-4 shrink-0 -translate-y-px transition-all group-hover:translate-x-2 group-focus-visible:translate-x-2" />
+      </TextLink>
     </SectionSticky>
-   </ul>
-   <TextLink innerText="Ver todos os projetos">
-    <GoProjectSymlink className="inline-block h-4 w-4 shrink-0 -translate-y-px transition-all group-hover:translate-x-2 group-focus-visible:translate-x-2" />
-   </TextLink>
-  </SectionSticky>
- );
+  );
 }
--- src/components/_components/Card/index.jsx
+++ src/components/_components/Card/index.jsx
@@ -5,86 +5,74 @@ import Tag from "../Tag/tag";
 import { Paragraph } from "../Paragraph";
 
 export default function Card({
- href,
- innerImage,
- innerDate,
- innerTitle,
- innerSubTitle,
- innerDescription,
- tags,
+  href,
+  innerDate,
+  innerTitle,
+  innerSubTitle,
+  innerDescription,
+  tags,
 }) {
- const previewDescription = targetDescription(innerDescription, 120);
+  const previewDescription = targetDescription(innerDescription, 200);
 
- function targetDescription(text, maxLength) {
-  if (text.length > maxLength) {
-   return text.substring(0, maxLength) + "...";
-  } else {
-   return text;
+  function targetDescription(text, maxLength) {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    } else {
+      return text;
+    }
   }
- }
 
- return (
-  <li className="mb-12">
-   {/** Componente usado nas páginas: formação e projetos */}
-   <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:hover:!scale-110 lg:group-hover/list:opacity-50 lg:group-hover/list:scale-90">
-    <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition-all motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+  return (
+    <li className="mb-12">
+      {/** Componente usado nas páginas: formação e projetos */}
+      <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:hover:!scale-110 lg:group-hover/list:opacity-50 lg:group-hover/list:scale-90">
+        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition-all motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
 
-    {innerDate && (
-     <div className="z-10 text-xs font-semibold text-slate-500 sm:col-span-2">{innerDate}</div>
-    )}
-
-    <div className="z-10 sm:order-2 sm:col-span-6">
-     <Link
-      className={`${
-       href === "" && "cursor-default"
-      } flex items-baseline font-medium leading-tight text-slate-200 transition-all hover:text-teal-300 focus-visible:text-teal-300 group/link text-base`}
-      href={href}
-      scroll={false}
-     >
-      <div className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></div>
-      <span className="w-full">
-       <div className={`flex items-center ${!innerSubTitle && "pb-2"}`}>
-        {innerTitle}
-        {href != "" && (
-         <BiRightIndent className="hidden h-auto w-6 shrink-0 transition-all group-hover/link:translate-x-full group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 ml-1 lg:block" />
+        {innerDate && (
+          <div className="z-10 text-xs font-semibold text-slate-500 sm:col-span-2">
+            {innerDate}
+          </div>
         )}
-       </div>
-       {innerSubTitle && <p className="text-slate-500 pb-2">{innerSubTitle}</p>}
-      </span>
-     </Link>
 
-     {href === "" ? (
-      <Paragraph className="text-sm" text={innerDescription} />
-     ) : (
-      <Paragraph text={previewDescription} />
-     )}
+        <div className="z-10 sm:order-2 sm:col-span-6">
+          <Link
+            className={`${
+              href === "" && "cursor-default"
+            } flex items-baseline font-medium leading-tight text-slate-200 transition-all hover:text-teal-300 focus-visible:text-teal-300 group/link text-base`}
+            href={href}
+            scroll={false}
+          >
+            <div className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></div>
+            <span className="w-full">
+              <div className={`flex items-center ${!innerSubTitle && "pb-2"}`}>
+                {innerTitle}
+                {href != "" && (
+                  <BiRightIndent className="hidden h-auto w-6 shrink-0 transition-all group-hover/link:translate-x-full group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 ml-1 lg:block" />
+                )}
+              </div>
+              {innerSubTitle && (
+                <p className="text-slate-500 pb-2">{innerSubTitle}</p>
+              )}
+            </span>
+          </Link>
 
-     {tags && (
-      <ul className="mt-4 flex gap-2 flex-wrap">
-       {tags.map((tag, index) => (
-        <li key={index}>
-         <Tag innerText={tag} />
-        </li>
-       ))}
-      </ul>
-     )}
-    </div>
+          {href === "" ? (
+            <Paragraph className="text-sm" text={innerDescription} />
+          ) : (
+            <Paragraph text={previewDescription} />
+          )}
 
-    {innerImage && (
-     <Link href={href} className="relative sm:order-1 sm:col-span-2 sm:translate-y-1">
-      <Image
-       src={innerImage}
-       width={500}
-       height={500}
-       alt="Pequena imagem do projeto"
-       className="relative rounded border-2 border-slate-200/10 transition-all group-hover:border-slate-200/30"
-      />
-      <span className="lg:hidden text-xs absolute bottom-2 right-2 bg-black/60 rounded p-2">
-       Toque para ver mais
-      </span>
-     </Link>
-    )}
-   </div>
-  </li>
- );
+          {tags && (
+            <ul className="mt-4 flex gap-2 flex-wrap">
+              {tags.map((tag, index) => (
+                <li key={index}>
+                  <Tag innerText={tag} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </li>
+  );
 }
