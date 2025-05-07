2bee1376d6e02b4332c34a91f0e189776ae17571
walacecordeiro <walacecordeirodossantos@gmail.com>
2025-05-06 22:44:05 -0300

    comentei a página dinamica dos projetos para fazer o deploy apenas, pois estava com erro por causa de alguns arquivos excluiros; não deletei pois planejo usar

--- src/app/projetos/[details]/page.jsx
+++ src/app/projetos/[details]/page.jsx
@@ -1,51 +1,51 @@
-"use client";
+// "use client";
 
-import Image from "next/image";
-import { data } from "../data";
-import { useRouter } from "next/navigation";
+// import Image from "next/image";
+// import { data } from "../data";
+// import { useRouter } from "next/navigation";
 
-import {
- Carousel,
- CarouselContent,
- CarouselItem,
- CarouselNext,
- CarouselPrevious,
-} from "@/components/ui/carousel";
-import { SectionSticky } from "@/components/_components/SectionSticky";
+// import {
+//  Carousel,
+//  CarouselContent,
+//  CarouselItem,
+//  CarouselNext,
+//  CarouselPrevious,
+// } from "@/components/ui/carousel";
+// import { SectionSticky } from "@/components/_components/SectionSticky";
 
-export default function Page({ params }) {
- const router = useRouter();
- const project = data.find((project) => project.href.includes(params.details));
+// export default function Page({ params }) {
+//  const router = useRouter();
+//  const project = data.find((project) => project.href.includes(params.details));
 
- if (!data.some((item) => item.href.includes(params.details))) {
-  router.push("/404");
-  return null;
- }
+//  if (!data.some((item) => item.href.includes(params.details))) {
+//   router.push("/404");
+//   return null;
+//  }
 
- return (
-  <SectionSticky titleStickyOnMobile={project && project.innerTitle} normalCase>
-   <Carousel>
-    <CarouselContent>
-     {project.images.map((image) => (
-      <CarouselItem key={image} className="flex basis-5/6 lg:basis-full">
-       <Image
-        priority
-        width={800}
-        height={800}
-        src={image}
-        alt="Imagem do projeto"
-        className="rounded border-2 border-slate-200/30 transition-all group-hover:border-slate-200/30"
-       />
-      </CarouselItem>
-     ))}
-    </CarouselContent>
-    <CarouselPrevious className="hidden text-primary bg-black/60 transition-all lg:-left-12 lg:grid lg:hover:scale-125" />
-    <CarouselNext className="hidden text-primary bg-black/60  transition-all lg:-right-12 lg:grid lg:hover:scale-125" />
-   </Carousel>
-   <h2 className="hidden mt-3 text-md font-medium tracking-tight text-slate-200 sm:text-xl lg:block">
-    {project.innerTitle}
-   </h2>
-   <p className="mt-2 text-sm leading-normal">{project.innerDescription}</p>
-  </SectionSticky>
- );
-}
+//  return (
+//   <SectionSticky titleStickyOnMobile={project && project.innerTitle} normalCase>
+//    <Carousel>
+//     <CarouselContent>
+//      {project.images.map((image) => (
+//       <CarouselItem key={image} className="flex basis-5/6 lg:basis-full">
+//        <Image
+//         priority
+//         width={800}
+//         height={800}
+//         src={image}
+//         alt="Imagem do projeto"
+//         className="rounded border-2 border-slate-200/30 transition-all group-hover:border-slate-200/30"
+//        />
+//       </CarouselItem>
+//      ))}
+//     </CarouselContent>
+//     <CarouselPrevious className="hidden text-primary bg-black/60 transition-all lg:-left-12 lg:grid lg:hover:scale-125" />
+//     <CarouselNext className="hidden text-primary bg-black/60  transition-all lg:-right-12 lg:grid lg:hover:scale-125" />
+//    </Carousel>
+//    <h2 className="hidden mt-3 text-md font-medium tracking-tight text-slate-200 sm:text-xl lg:block">
+//     {project.innerTitle}
+//    </h2>
+//    <p className="mt-2 text-sm leading-normal">{project.innerDescription}</p>
+//   </SectionSticky>
+//  );
+// }
